b8dd2fede4dab2d492e841b99269c0ea0604fd67
monumarquis <monumarquis@gmail.com>
2023-01-19 16:55:56 +0530

    Added

--- src/features/user/user.route.js
+++ src/features/user/user.route.js
@@ -7,8 +7,8 @@ const SECRET_TOKEN = process.env.SECRET_TOKEN;
 const SECRET_REFRESH_TOKEN = process.env.SECRET_REFRESH_TOKEN;
 
 // signup route
-app.get("/",()=>{
-res.send("User route")
+app.get("/",(req,res)=>{
+return res.send("User route")
 })
 app.post('/signup',async (req,res)=>{
     const { username, email, password } = req.body
