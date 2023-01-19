6f0b2c9a24f0385739c508211cb9a99659790194
monumarquis <monumarquis@gmail.com>
2023-01-19 16:33:15 +0530

    Added

--- src/index.js
+++ src/index.js
@@ -4,13 +4,13 @@ const cors = require('cors')
 const connect  = require('./config/db')
 const app = express()
 const User = require("./features/user/user.route")
+const PORT = process.env.PORT || 8080
 
 app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }));
 
 app.use("/user",User)
-const PORT = process.env.PORT || 8080
 
 app.get('/',(req,res)=>{
     res.send("This is Mock 11 Backend")
